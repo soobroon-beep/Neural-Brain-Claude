9035807a023984e3f7442426cc8980a8ed16e73b
VXTRSBXN <soobroon@gmail.com>
2026-06-23 21:15:20 -0600

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-
-const CACHE_NAME='neural-brain-v1';
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(clients.claim()));
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
